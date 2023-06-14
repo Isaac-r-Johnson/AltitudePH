b8dc064caa326b0ec16d131da72506c25c729bfd
Isaac Johnson <isaacrj.mk@gmail.com>
2023-06-14 20:08:20 +0800

    Add product popup window.

--- index/src/product.js
+++ index/src/product.js
@@ -1,4 +1,5 @@
 import './css/product.css';
+import Popup from 'reactjs-popup';
 
 const Product = (props) => {
     var pic = props.pic;
@@ -7,16 +8,52 @@ const Product = (props) => {
     var price = props.price;
     var des = props.des;
     return (
-        <div className='sizing'>
-            <div className="product-card">
-                <img className='coffee-pic' src={pic} alt={title}/>
-                <div className='product-info'>
-                    <h5>{title}</h5>
-                    <h6>{cat}</h6>
-                    <h4>₱{price}.00</h4>
-                    <p>{des}</p>
-                </div>
-            </div>
+        <div>
+            <Popup trigger=
+                    {
+                    <div className='sizing'>
+                        <div className="product-card">
+                            <img className='coffee-pic' src={pic} alt={title}/>
+                            <div className='product-info'>
+                                <h5>{title}</h5>
+                                <h6>{cat}</h6>
+                                <h4>₱{price}.00</h4>
+                                <p>{des}</p>
+                            </div>
+                        </div>
+                    </div>
+                    }
+                    modal nested>
+                    {
+                        close => (
+                            <div className='popup'>
+                                <nav className="navbar">
+                                    <div className="container-fluid">
+                                        <div className="">
+                                            <button onClick={() => { close()}} className="back-btn">Go Back</button>
+                                        </div>
+                                        <button id="logo-btn"><img src="logo.jpg" alt="AltitudePH" width="70" height="70" className="d-block align-text-top logo"/></button>
+                                    </div>
+                                </nav>
+                                <div className='meat'>
+                                    <img className='display-img' src={pic} alt={title}></img>
+                                    <div className='separator'></div>
+                                    <div className='product-info-card'>
+                                        <div className='content'>
+                                            <h6>{cat}</h6>
+                                            <h3>{title}</h3>
+                                            <h5>₱{price}.00</h5>
+                                        </div>   
+                                        <hr/>
+                                        <div className='content'>
+                                            <p>{des}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+            </Popup>
         </div>
     );
 }
