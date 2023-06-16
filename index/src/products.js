e0cabc0392dabfb17f9c3e9116a559425415b6fc
Isaac Johnson <isaacrj.mk@gmail.com>
2023-06-16 16:11:11 +0800

    Make image rendering for efficient and easier to understand

--- index/src/products.js
+++ index/src/products.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import './css/products.css';
 import Product from './product';
 import React from 'react';
@@ -19,117 +20,112 @@ const App = (props) => {
     React.useEffect(() => {
         LoadProducts();
       }, []);
-
-    console.log("rendering");
-    if (display === "all" && viewType === "insta" && sortType === "cat"){
-        // Also use vercel to host backend, then change address in the package.json for react app
-        return (
-            <div>
-                <div className='container'>
-                    <h2>All Items</h2>
-                    <div className='row holder'>
-                    {
-                        products.map((product) => {
-                            const base64String = btoa(
-                                String.fromCharCode(...new Uint8Array((product.img.data.data)))
-                            );
-                            return <div className='col-md'><Product pic={`data:image/png;base64,${base64String}`} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
-                        })
-                    }
+    
+    if (viewType === "insta"){
+        if (display === "all" && sortType === "cat"){
+            // Also use vercel to host backend, then change address in the package.json for react app
+            return (
+                <div>
+                    <div className='container'>
+                        <h2>All Items</h2>
+                        <div className='row holder'>
+                        {
+                            products.map((product) => {
+                                return <div className='col-md'><Product pic={product.img} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
+                            })
+                        }
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else if (display === "food" && sortType === "cat"){
+            // Also use vercel to host backend, then change address in the package.json for react app
+            return (
+                <div>
+                    <div className='container'>
+                        <h2>Food</h2>
+                        <div className='row holder'>
+                        {
+                            products.map((product) => {
+                                if (product.cat === "food"){
+                                    return <div className='col-md'><Product pic={product.img} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
+                                }
+                            })
+                        }
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else if (display === "bottled" && sortType === "cat"){
+            // Also use vercel to host backend, then change address in the package.json for react app
+            return (
+                <div>
+                    <div className='container'>
+                        <h2>Bottled Drinks</h2>
+                        <div className='row holder'>
+                        {
+                            products.map((product) => {
+                                if (product.cat === "bottled"){
+                                    return <div className='col-md'><Product pic={product.img} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
+                                }
+                            })
+                        }
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else if (display === "hot" && sortType === "cat"){
+            // Also use vercel to host backend, then change address in the package.json for react app
+            return (
+                <div>
+                    <div className='container'>
+                        <h2>Hot Drinks</h2>
+                        <div className='row holder'>
+                        {
+                            products.map((product) => {
+                                if (product.cat === "hot"){
+                                    return <div className='col-md'><Product pic={product.img} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
+                                }
+                            })
+                        }
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else if (display === "tea" && sortType === "cat"){
+            // Also use vercel to host backend, then change address in the package.json for react app
+            return (
+                <div>
+                    <div className='container'>
+                        <h1>Tea</h1>
+                        <div className='row holder'>
+                        {
+                            products.map((product) => {
+                                if (product.cat === "tea"){
+                                    return <div className='col-md'><Product pic={product.img} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
+                                }
+                            })
+                        }
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
-    else if (display === "food" && viewType === "insta" && sortType === "cat"){
-        // Also use vercel to host backend, then change address in the package.json for react app
-        return (
-            <div>
-                <div className='container'>
-                    <h2>Food</h2>
-                    <div className='row holder'>
-                    {
-                        products.map((product) => {
-                            const base64String = btoa(
-                                String.fromCharCode(...new Uint8Array((product.img.data.data)))
-                            );
-                            if (product.cat === "food"){
-                                return <div className='col-md'><Product pic={`data:image/png;base64,${base64String}`} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
-                            }
-                        })
-                    }
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else if (display === "bottled" && viewType === "insta" && sortType === "cat"){
-        // Also use vercel to host backend, then change address in the package.json for react app
-        return (
-            <div>
-                <div className='container'>
-                    <h2>Bottled Drinks</h2>
-                    <div className='row holder'>
-                    {
-                        products.map((product) => {
-                            const base64String = btoa(
-                                String.fromCharCode(...new Uint8Array((product.img.data.data)))
-                            );
-                            if (product.cat === "bottled"){
-                                return <div className='col-md'><Product pic={`data:image/png;base64,${base64String}`} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
-                            }
-                        })
-                    }
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else if (display === "hot" && viewType === "insta" && sortType === "cat"){
-        // Also use vercel to host backend, then change address in the package.json for react app
-        return (
-            <div>
-                <div className='container'>
-                    <h2>Hot Drinks</h2>
-                    <div className='row holder'>
-                    {
-                        products.map((product) => {
-                            const base64String = btoa(
-                                String.fromCharCode(...new Uint8Array((product.img.data.data)))
-                            );
-                            if (product.cat === "hot"){
-                                return <div className='col-md'><Product pic={`data:image/png;base64,${base64String}`} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
-                            }
-                        })
-                    }
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else if (display === "tea" && viewType === "insta" && sortType === "cat"){
-        // Also use vercel to host backend, then change address in the package.json for react app
-        return (
-            <div>
-                <div className='container'>
-                    <h1>Tea</h1>
-                    <div className='row holder'>
-                    {
-                        products.map((product) => {
-                            const base64String = btoa(
-                                String.fromCharCode(...new Uint8Array((product.img.data.data)))
-                            );
-                            if (product.cat === "tea"){
-                                return <div className='col-md'><Product pic={`data:image/png;base64,${base64String}`} title={product.name} cat={product.cat} price={product.price} des={product.des}/></div>
-                            }
-                        })
-                    }
-                    </div>
-                </div>
-            </div>
-        );
+    else if (viewType === "list"){
+        console.log("A Work In Progress!");
     }
 
 }
 
 export default App;
+
+// If I need images again.
+// const base64String = btoa(
+//     String.fromCharCode(...new Uint8Array((product.img.data.data)))
+// );
+// {`data:image/png;base64,${base64String}`}
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const fs = require('node:fs');
-const multer = require('multer');
-const path = require('path');
+// const multer = require('multer');
+// const path = require('path');
 
 // Init
 const app = express();
@@ -12,25 +12,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: true }));
 PORT = 5000;
 
-const clear = (dir) => {
-    fs.readdir(dir, (err, files) => {
-        if (err) throw err;
+// const clear = (dir) => {
+//     fs.readdir(dir, (err, files) => {
+//         if (err) throw err;
       
-        for (const file of files) {
-          fs.unlink(path.join(dir, file), (err) => {
-            if (err) throw err;
-          });
-        }
-      });
-}
+//         for (const file of files) {
+//           fs.unlink(path.join(dir, file), (err) => {
+//             if (err) throw err;
+//           });
+//         }
+//       });
+// }
 
 
 // DB Prep
 const productSchema = new mongoose.Schema({
-    img: {
-        data: Buffer,
-        contentType: String
-    },
+    // img: {
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    img: String,
     cat: String,
     name: String,
     price: Number,
@@ -38,228 +39,199 @@ const productSchema = new mongoose.Schema({
 });
 const Product = mongoose.model("Product", productSchema);
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, './uploads');
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.originalname);
-    }
-});
-
-const upload = multer({storage:storage});
-
-// listen for connections
-fs.readFile(__dirname + "/key.txt", 'utf8', (err, addr) => {
-    mongoose.connect(addr)
-    .then(() => {
-        app.listen(PORT, () => {
-        console.log("Connected to DB & Listening on port " + PORT + "...");
-    });
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+app.get('/', (req, res) => {
+    res.send("It is working!");
 });
 
 // Temp URL to write products to the db
 const products = [
     {
-        img: "",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901054/AltitudePHImages/coco-brew_owutee.jpg",
         cat: "bottled",
         name: "Coco Brew",
         price: 150,
         des: "Espresso + coconut milk + desiccated coconut. We know this is kind of a weird combination for most people. But you gotta trust us on this."
     },
     {
-        img: "../images/cold-brew.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901054/AltitudePHImages/cold-brew_sexcs2.jpg",
         cat: "bottled",
         name: "Cold Brew",
         price: 120,
         des: "Coffee steeped in cold water for 24 hours. If you want a more relaxed black coffee, Cold Brew is the way to go. "
     },
     {
-        img: "../images/dirty-matcha-latte.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901054/AltitudePHImages/dirty-matcha-latte_ngqolx.jpg",
         cat: "bottled",
         name: "Dirty Matcha Latte",
         price: 150,
         des: "Espresso + matcha + milk, but served cold. "
     },
     {
-        img: "../images/iced-choco.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/iced-choco_andqwt.jpg",
         cat: "bottled",
         name: "Iced Choco",
         price: 140,
         des: "A hot chocolate but make it cold."
     },
     {
-        img: "../images/matcha-latte.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/matcha-latte_oedixo.jpg",
         cat: "bottled",
         name: "Matcha Latte",
         price: 140,
         des: ""
     },
     {
-        img: "../images/mocha-brew.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/mocha-brew_vidw4v.jpg",
         cat: "bottled",
         name: "Mocha Brew",
         price: 150,
         des: "Espresso + chocolate + milk, served cold. "
     },
     {
-        img: "../images/white-brew.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901057/AltitudePHImages/white-brew_lqgwo0.jpg",
         cat: "bottled",
         name: "White Brew",
         price: 140,
         des: "Espresso + milk. Served cold. Basically a cold Latte."
     },
     {
-        img: "../images/mini-pizza-focaccia.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/mango-matcha-latte_t67cvj.jpg",
+        cat: "bottled",
+        name: "Mango Matcha Latte",
+        price: 150,
+        des: ""
+    },
+    {
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/mini-pizza-focaccia_c6dvgf.jpg",
         cat: "food",
         name: "Mini Pizza Focaccia",
         price: 80,
         des: ""
     },
     {
-        img: "../images/americano.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901054/AltitudePHImages/americano_rgasql.jpg",
         cat: "hot",
         name: "Americano",
         price: 100,
         des: "Just espresso and water."
     },
     {
-        img: "../images/cortado.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901054/AltitudePHImages/cortado_pou8fk.jpg",
         cat: "hot",
         name: "Cortado",
         price: 100,
         des: "Espresso + milk, but smaller than a flat white."
     },
     {
-        img: "../images/dirty-matcha-latte-hot.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/dirty-matcha-latte-hot_uinlvj.jpg",
         cat: "hot",
         name: "Dirty Matcha Latte",
         price: 130,
         des: "Espresso + matcha + milk. When you love coffee and matcha at the same time, but can't decide which one to drink. Drink them both."
     },
     {
-        img: "../images/duet.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/duet_l6ug9i.jpg",
         cat: "hot",
         name: "Duet",
         price: 160,
         des: ""
     },
     {
-        img: "../images/espresso.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/espresso_skz9qc.jpg",
         cat: "hot",
         name: "Espresso",
         price: 100,
         des: "Just espresso, for the cold-hearted."
     },
     {
-        img: "../images/flat-white.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/flat-white_ejdvxk.jpg",
         cat: "hot",
         name: "Flat White",
         price: 110,
         des: "Espresso + milk, but smaller than a latte."
     },
     {
-        img: "../images/hot-chocolate.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/hot-chocolate_n6frdh.jpg",
         cat: "hot",
         name: "Hot Chocolate",
         price: 120,
         des: "Chocolate + steamed milk. For the kids, but if you're a kid at heart, you can get this too."
     },
     {
-        img: "../images/latte.jpg",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/latte_f8seft.jpg",
         cat: "hot",
         name: "Latte",
         price: 120,
         des: "Espresso + steamed milk. The basic drink of all but it's the best."
     },
     {
-        img: "../images/lungo.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/lungo_cioarx.jpg",
         cat: "hot",
         name: "Lungo",
         price: 100,
         des: ""
     },
     {
-        img: "../images/matcha-latte-hot.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/matcha-latte-hot_rlgbzz.jpg",
         cat: "hot",
         name: "Matcha Latte",
         price: 120,
         des: ""
     },
     {
-        img: "../images/mocha-latte-hot.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/mocha-latte-hot_bqgyxy.jpg",
         cat: "hot",
         name: "Mocha Latte",
         price: 130,
         des: ""
     },
     {
-        img: "../images/oatmilk-flat-white.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901057/AltitudePHImages/oatmilk-flat-white_n2qcca.jpg",
         cat: "hot",
         name: "Oatmilk Flat White",
         price: 130,
         des: ""
     },
     {
-        img: "../images/oatmilk-latte.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901056/AltitudePHImages/oatmilk-latte_alqzhu.jpg",
         cat: "hot",
         name: "Oatmilk Latte",
         price: 140,
         des: ""
     },
     {
-        img: "../images/piccolo.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901057/AltitudePHImages/piccolo_rhbqbw.jpg",
         cat: "hot",
         name: "Piccolo",
         price: 100,
         des: ""
     },
     {
-        img: "../images/pour-over-coffee.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901057/AltitudePHImages/pour-over-coffee_ygvy7v.jpg",
         cat: "hot",
         name: "Pour Over Coffee",
         price: 100,
         des: ""
     },
     {
-        img: "../images/spanish-latte.JPG",
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901057/AltitudePHImages/spanish-latte_dss8vo.jpg",
         cat: "hot",
         name: "Spanish Latte",
         price: 120,
         des: ""
+    },
+    {
+        img: "https://res.cloudinary.com/dqaxkucbu/image/upload/v1686901055/AltitudePHImages/iced-lemon-black_ragkv7.jpg",
+        cat: "tea",
+        name: "Iced Lemon Black Tea",
+        price: 85,
+        des: ""
     }
 ]
-
-app.post('/upload', upload.single('productImg'), (req, res) => {
-    const productToUpload = {
-        img:{
-            data: fs.readFileSync('./uploads/' + req.file.filename),
-            contentType: 'image/png'
-        },
-        cat: req.body.cat,
-        name: req.body.name,
-        price: Number(req.body.price),
-        des: req.body.des
-    }
-    Product.insertMany(productToUpload)
-    .then(() => {
-        res.send("Good!");
-        clear('./uploads');
-    })
-    .catch(() => {
-        res.send("Bad!");
-        clear('./uploads');
-    })
+app.get('/write', (req, res) => {
+    Product.insertMany(products)
+    .then(res.send("Success!!"));
 });
-
-app.get('/', (req, res) => {
-    res.send("It is working!");
-});
-
 
 const prepareProducts = (data) => {
     var productsList = [];
@@ -281,5 +253,55 @@ app.get('/products', (req, res) => {
     });
 });
 
+
+// listen for connections
+fs.readFile(__dirname + "/key.txt", 'utf8', (err, addr) => {
+    mongoose.connect(addr)
+    .then(() => {
+        app.listen(PORT, () => {
+        console.log("Connected to DB & Listening on port " + PORT + "...");
+    });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+});
+
+
+// use for images if this doesn't work:
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, './uploads');
+//     },
+//     filename: (req, file, cb)=>{
+//         cb(null, file.originalname);
+//     }
+// });
+
+// const upload = multer({storage:storage});
+
+
+// app.post('/upload', upload.single('productImg'), (req, res) => {
+//     const productToUpload = {
+//         img:{
+//             data: fs.readFileSync('./uploads/' + req.file.filename),
+//             contentType: 'image/png'
+//         },
+//         cat: req.body.cat,
+//         name: req.body.name,
+//         price: Number(req.body.price),
+//         des: req.body.des
+//     }
+//     Product.insertMany(productToUpload)
+//     .then(() => {
+//         res.send("Good!");
+//         clear('./uploads');
+//     })
+//     .catch(() => {
+//         res.send("Bad!");
+//         clear('./uploads');
+//     })
+// });
 
 
